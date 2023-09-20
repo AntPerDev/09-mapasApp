0653b95fb799dd35844a892f54465f464e579943
Antonio Pérez <64748202+AntPerDev@users.noreply.github.com>
2023-09-20 06:32:59 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  mapboxToken: 'pk.eyJ1IjoiYW50cGVyIiwiYSI6ImNrdDBjaWJlaDF5b2MzMXB1aGpvbnF3azEifQ.lCY8w_DIAl6riHTBZQd9Lg'
+  mapboxToken: 'GoToMapBoxCreateYourOwnTokenAndUseItanddoNotexposeItOnGitHubLearn.Env'
 
 };
